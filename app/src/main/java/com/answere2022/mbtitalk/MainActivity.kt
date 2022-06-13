74b2ba7ed53d3ed11c2c170c05519b905d172785
answere3030 <answere3030@gmail.com>
2022-06-13 17:08:00 +0900

    2022.06.13 - ver 0.1

---
+++ app/src/main/java/com/answere2022/mbtitalk/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.answere2022.mbtitalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
